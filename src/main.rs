f5ff9e3bcdbb46b7be8efa8a6d31af0660fa5a89
unknown <trv6775@gmail.com>
2026-07-27 11:21:49 +0300

    Using the cubesat ground station to understand error handling this will compile but later iterations like the next introduces us to ownership and how you must use borrowing and lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,27 @@
-#[derive(Debug, PartialEq)]
-enum FileState {
-    Open,
-    Closed,
-}
+#![allow(unused_variables)]
 
 #[derive(Debug)]
-struct File {
-    name: String,
-    data: Vec<u8>,
-    state: FileState,
+enum StatusMessage {
+    Ok,
 }
 
-impl File {
-    fn new(name: &str) -> File {
-        File {
-            name: String::from(name),
-            data: Vec::new(),
-            state: FileState::Closed,
-        }
-    }
-
-    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
-        if self.state != FileState::Open {
-            return Err(String::from("File must be open for reading"));
-        }
-        let mut tmp = self.data.clone();
-        let read_length = tmp.len();
-        save_to.reserve(read_length);
-        save_to.append(&mut tmp);
-        Ok(read_length)
-    }
-}
-
-fn open(mut f: File) -> Result<File, String> {
-    f.state = FileState::Open;
-    Ok(f)
-}
-
-fn close(mut f: File) -> Result<File, String> {
-    f.state = FileState::Closed;
-    Ok(f)
+fn check_status(sat_id: u64) -> StatusMessage {
+    StatusMessage::Ok
 }
 
 fn main() {
-    let mut f5 = File::new("5.txt");
-    let mut buffer: Vec<u8> = vec![];
+    let sat_a = 0;
+    let sat_b = 1;
+    let sat_c = 2;
 
-    if f5.read(&mut buffer).is_err() {
-        println!("Error checking is working");
-    }
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
-    f5 = open(f5).unwrap();
-    let f5_length = f5.read(&mut buffer).unwrap();
-    f5 = close(f5).unwrap();
-
-    let text = String::from_utf8_lossy(&buffer);
-
-    println!("{:?}", f5);
-    println!("{} is {} bytes long", &f5.name, f5_length);
-    println!("{}", text);
+    // "waiting" ...
+    let a_status = check_status(sat_a);
+    let b_status = check_status(sat_b);
+    let c_status = check_status(sat_c);
+    println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 }
